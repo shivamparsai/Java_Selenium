231bfd813ce98499ab9e4e54ce96f458b9b87c44
shivam <parsaishivam@gmail.com>
2022-07-09 16:29:54 +0530

    commit

---
+++ ProgramPractice/src/main/java/com/practice/Hello.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:/selenium software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		
+	}
+
+}
